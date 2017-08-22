d4167c8bd1e6dbde2a537e7b0bf8f3100c3f7a31
allan <15989110619@139.com>
2017-08-22 11:12:42 +0800

    struts上传下载文件

--- src/main/java/com/gdglc/spring/struts/controller/auth/UserAction.java
+++ src/main/java/com/gdglc/spring/struts/controller/auth/UserAction.java
@@ -1,13 +1,53 @@
 package com.gdglc.spring.struts.controller.auth;
 
-import com.gdglc.spring.SpringHelper;
-import com.gdglc.spring.biz.auth.IUserBiz;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URLEncoder;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.struts2.ServletActionContext;
+
 import com.gdglc.spring.exception.BizException;
-import com.opensymphony.xwork2.ActionContext;
 
 public class UserAction {
 	private String name;
+	private File file;
+	private String fileContentType;
+	private String fileFileName;
+	private InputStream inputStream;
+	private String downFileName;
 	
+	public File getFile() {
+		return file;
+	}
+
+	public void setFile(File file) {
+		this.file = file;
+	}
+
+	public String getFileContentType() {
+		return fileContentType;
+	}
+
+	public void setFileContentType(String fileContentType) {
+		this.fileContentType = fileContentType;
+	}
+
+	public String getFileFileName() {
+		return fileFileName;
+	}
+
+	public void setFileFileName(String fileFileName) {
+		this.fileFileName = fileFileName;
+	}
+
 	public String getName() {
 		return name;
 	}
@@ -16,28 +56,128 @@ public class UserAction {
 		this.name = name;
 	}
 
-	public String showList(){
-		System.out.println("---UserAction showList---"+this.name);
+	public InputStream getInputStream() {
+		return inputStream;
+	}
+
+	public void setInputStream(InputStream inputStream) {
+		this.inputStream = inputStream;
+	}
+
+	public String getDownFileName() {
+		return downFileName;
+	}
+
+	public void setDownFileName(String downFileName) {
+		this.downFileName = downFileName;
+	}
+
+	public String showList() {
+		System.out.println("---UserAction showList---" + this.name);
 		return "list";
 	}
-	
-	public String add(){
+
+	public String add() {
 		System.out.println("---UserAction add---");
 		return "add";
 	}
-	
-	public String delete(){
+
+	public String delete() {
 		System.out.println("---UserAction delete---");
 		return "test";
 	}
-	
-	public String update(){
+
+	public String update() {
 		return "update";
 	}
-	
-	public String showInfo(){
+
+	public String showInfo() {
+		return "info";
+	}
+
+	public String upload() {
+		System.out.println(this.file);
+		System.out.println(getFileContentType());
+		System.out.println(getFileFileName());
+		try {
+			uploadFile(file, fileFileName, "/upload/", "test");
+		} catch (BizException e) {
+			e.printStackTrace();
+		}
 		return "info";
 	}
 	
-	
+	public String down(){
+		String filePath = "";
+		// 获取项目真实路径
+		String realPath = ServletActionContext.getServletContext().getRealPath("/");
+		filePath= realPath+File.separator+"upload"+File.separator+"test.jpg";		
+		try {
+			inputStream = new BufferedInputStream(new FileInputStream(filePath));
+			this.setDownFileName(URLEncoder.encode("测试.jpg","utf-8"));
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("不存在该文件");
+			//你们应该自己去决定如何提示用户，这里的none只是测试使用
+			return "none";
+		}
+		return "down";
+	}
+
+	private String uploadFile(File file, String fileFileName, String path,String fileName) throws BizException {
+		// 获取项目真实路径
+		String realPath = ServletActionContext.getServletContext().getRealPath("/");
+		System.out.println(realPath);
+		String fileType = "";
+		if (StringUtils.isBlank(fileFileName)) {
+			return null;
+		}
+		BufferedInputStream bs = null;
+		BufferedOutputStream bos = null;
+		// 获取文件后缀 a.txt.xml jpg
+		fileType = fileFileName.substring(fileFileName.lastIndexOf('.')).toLowerCase();
+		// 判断文件格式
+		fileName = fileName + fileType;// 文件名小写处理
+		try {
+			File pathFile = new File(realPath + path);
+			if (!pathFile.exists() || !pathFile.isDirectory()) {
+				pathFile.mkdirs();
+			}
+			InputStream in = new FileInputStream(file);
+			OutputStream out = new FileOutputStream(new File(realPath + path+ fileName));
+			bs = new BufferedInputStream(in);
+			bos = new BufferedOutputStream(out);
+			byte[] buffer = new byte[2048];
+			while ((bs.read(buffer)) != -1) {
+				bos.write(buffer);
+				bos.flush();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new BizException("保存文件失败!");
+		} finally {
+			if (bs != null) {
+				try {
+					bs.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}finally{
+					bs = null;
+				}
+			}
+			if (bos != null) {
+				try {
+					bos.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}finally{
+					bos = null;
+				}
+			}
+		}
+		return fileType;
+	}
+	public static void main(String[] args) {
+		System.out.println(File.separator);
+	}
 }
--- src/main/webapp/jsp/index.jsp
+++ src/main/webapp/jsp/index.jsp
@@ -1,5 +1,27 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@taglib prefix="c"  uri="http://java.sun.com/jsp/jstl/core"%>
+   
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>上传文件</title>
+</head>
+<%
+	request.setAttribute("path", request.getContextPath());
+%>
 <body>
-9999
+<form id="userForm" action="${path}/auth_User_upload" enctype="multipart/form-data" method="post" target="frame">
+		<label for="name">用户名：</label>
+		<input type="text" name="name" id="name"/>
+		<label for="age">年龄:</label>
+		<input type="text" name="age" id="age"/>
+		<input type="file" name="file"/>
+		
+		<input type="submit" value="提交">
+	</form>
+	<a href="${path}/auth_User_down">下载测试</a>
+	<iframe name="frame" src=""></iframe>
 </body>
 </html>
