9ee7cfbce27c77c10cf701f0ddfe5a9f3e291de3
allan <15989110619@139.com>
2017-09-11 12:49:19 +0800

    添加销毁钩子

--- src/main/java/com/gdglc/spring/dao/BaseDao.java
+++ src/main/java/com/gdglc/spring/dao/BaseDao.java
@@ -6,14 +6,35 @@ import org.hibernate.cfg.Configuration;
 
 public class BaseDao {
 	static SessionFactory sessionFactory;
-	
 	static{
 		Configuration configuration = new Configuration().configure("/hibernate/hibernate.xml");
 		//获取会话工厂
+		System.out.println("加载会话工厂");
 		sessionFactory = configuration.buildSessionFactory();
 	}
-	
+	static{
+		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("准备退出");
+				try{
+					if(null!=sessionFactory){
+						sessionFactory.close();
+					}
+				}catch(Exception e){
+					e.printStackTrace();
+				}finally{
+					sessionFactory = null;
+				}
+				System.out.println("退出成功");
+			}
+		}));
+	}
 	public Session getSession(){
 		return sessionFactory.openSession();
+	}
+	
+	public static void main(String[] args) {
+		//System.exit(0);
 	}
 }
